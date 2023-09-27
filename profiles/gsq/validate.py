2115ae99baa73f851f485d69dfcb8750fdc06b73
Nicholas Car <nick@kurrawong.net>
2023-09-27 21:24:22 +1000

    GSQ Profile validator v0.0.1 & 2 examples

--- profiles/gsq/validate.py
+++ profiles/gsq/validate.py
@@ -3,7 +3,7 @@ from rdflib import Graph
 from pyshacl import validate
 
 data = Graph().parse(sys.argv[1])
-data.update(open("oc-to-c.sparql").read())
+data.update(open("oc-to-o.sparql").read())
 
 v = validate(data, shacl_graph=sys.argv[2])
 
