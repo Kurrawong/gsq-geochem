2cc4cac95943c60631d121441237073edf8bf7ea
Nicholas Car <nick@kurrawong.net>
2023-11-20 23:21:53 +1000

    inc notations for CS & Col in concepts-combined.ttl

--- profiles/gsq/vocabs/combine_concepts_for_validation.py
+++ profiles/gsq/vocabs/combine_concepts_for_validation.py
@@ -16,5 +16,11 @@ for f in sorted(files):
             combined.add((c, RDF.type, SKOS.Concept))
             combined.add((c, SKOS.inScheme, o))
             combined.add((c, SKOS.notation, g.value(subject=c, predicate=SKOS.notation)))
+            combined.add((o, SKOS.notation, g.value(subject=o, predicate=SKOS.notation)))
+
+    for col in g.subjects(RDF.type, SKOS.Collection):
+        combined.add((col, SKOS.notation, g.value(subject=col, predicate=SKOS.notation)))
+        for mem in g.objects(col, SKOS.member):
+            combined.add((col, SKOS.member, mem))
 
 combined.serialize(OUTPUT_FILE, format="longturtle")
